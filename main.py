abb8b4609d3439b2a1c7fec5e48bc44adda50318
Akhil <akhilv.sajja@gmail.com>
2026-08-21 22:00:53 -0400

    i learned about how websockets and async functions work in the background

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
+
+message_table = []
 
 @app.get("/")
 def home():
@@ -9,4 +11,13 @@ def home():
 @app.get("/other")
 def main():
      return {"status": "true"}
+
+@app.websocket("/ws")
+async def echo(websocket: WebSocket):
+    await websocket.accept()
+    print("test to accept")
+    while True:
+        message = await websocket.receive_text()
+        message_table.append(message)
+        await websocket.send_text(f"message log: {message_table}")
 
